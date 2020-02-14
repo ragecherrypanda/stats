6e081128fb7e278c97bf0682eb50c2436dfd956c
banana <savannah.allsop@gmail.com>
2020-02-14 10:58:17 +0000

    changed supflags to maps instead of tuples, removed MACROs. Compiled and passed dialyzer

--- src/stats_sup.erl
+++ src/stats_sup.erl
@@ -1,70 +1,48 @@
-%%%-------------------------------------------------------------------
+%%%-----------------------------------------------------------------------------
 %%% @doc
-%%%
+%%% Supervisor to Start up the push_supervisor
 %%% @end
-%%%-------------------------------------------------------------------
+%%%-----------------------------------------------------------------------------
 -module(stats_sup).
 
 -behaviour(supervisor).
 
-%% API
 -export([start_link/0]).
-
-%% Supervisor callbacks
--export([init/1]).
+-export([init/1]). %% Supervisor callbacks
 
 -define(SERVER, ?MODULE).
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type, Timeout), {I, {I, start_link, []}, permanent, Timeout, Type, [I]}).
--define(CHILD(I, Type), ?CHILD(I, Type, 5000)).
-
-%%%===================================================================
+%%%=============================================================================
 %%% API functions
-%%%===================================================================
+%%%=============================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the supervisor
-%%
-%% @end
-%%--------------------------------------------------------------------
+%%------------------------------------------------------------------------------
 -spec(start_link() ->
     {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
 start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
-%%%===================================================================
+%%%=============================================================================
 %%% Supervisor callbacks
-%%%===================================================================
+%%%=============================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a supervisor is started using supervisor:start_link/[2,3],
-%% this function is called by the new process to find out about
-%% restart strategy, maximum restart frequency and child
-%% specifications.
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec(init(Args :: term()) ->
-    {ok, {SupFlags :: {RestartStrategy :: supervisor:strategy(),
-        MaxR :: non_neg_integer(), MaxT :: non_neg_integer()},
-        [ChildSpec :: supervisor:child_spec()]
-    }} |
-    ignore |
-    {error, Reason :: term()}).
 init([]) ->
     RestartStrategy = one_for_one,
     MaxRestarts = 5,
     MaxSecondsBetweenRestarts = 10,
 
-    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    SupFlags = #{strategy  => RestartStrategy,
+                 intensity => MaxRestarts,
+                 period    => MaxSecondsBetweenRestarts},
 
-    PushSup = ?CHILD(stats_push_sup, supervisor),
+    PushSup = #{id       => stats_push_sup,
+                start    => {stats_push_sup, start_link, []},
+                restart  => permanent,
+                shutdown => 5000,
+                type     => supervisor,
+                modules  => [stats_push_sup]},
 
     {ok, {SupFlags, [PushSup]}}.
 
-%%%===================================================================
+%%%=============================================================================
 %%% Internal functions
-%%%===================================================================
+%%%=============================================================================
